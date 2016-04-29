537b14832cf198d6611539ccee57eccdcf0e3e72
Shaowu Tseng <breeze101792@gmail.com>
2016-04-29 09:52:27 +0800

    init upload

---
+++ NCU Library/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NCU Library
+//
+//  Created by nculib on 2016/4/29.
+//  Copyright © 2016年 NCU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
