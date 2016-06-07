7a20b889bb5f0c73c61a2ef646e5d062bc911ade
Shaowu Tseng <shaowu@macbook-pro.local>
2016-06-07 11:12:02 +0800

    Add Alamofire submodule and get json data successfully.

--- NCU Library/ViewController.swift
+++ NCU Library/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    // MARK: Properties
+    @IBOutlet weak var searchBox: UITextField!
+    @IBOutlet weak var taLog: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +21,36 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispos of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func btnSearch(sender: UIButton) {
+        let url = "https://www2.lib.ncu.edu.tw/~nfsnfs/mobile-new/api/search?query=php"
+//        let url = "http://www.google.com"
+//        taLog.text = "test"
+//        Alamofire.request(.POST, url, parameters: parameters, encoding:.JSON)
+        Alamofire.request(.GET, url)
+            .responseJSON
+            { response in switch response.result {
+            case .Success(let JSON):
+                self.taLog.text = "success\n\(JSON)"
+//                self.taLog.text += JSON as! String
+//                print("Success with JSON: \(JSON)")
+//                
+//                let response = JSON as! NSDictionary
+//                
+//                //example if there is an id
+//                let userId = response.objectForKey("id")!
+                
+            case .Failure(let error):
+                self.taLog.text = "search error\(error)"
+                print("Request failed with error: \(error)")
+                }
+        }
+        
+        
     }
 
 
